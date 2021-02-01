318f1b77831c77f26366873161ca677ec80cdc0f
Katawut Utaipaisalwong <katawut@am2bmarketing.co.th>
2021-02-01 15:07:07 +0700

    update text to dev team

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello Am2b!
+          Hello Am2b Dev Team
         </p>
         <a
           className="App-link"
