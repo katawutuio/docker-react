e8693502ad1e417c21b44ea0ff32e12447faacbc
Katawut Utaipaisalwong <katawut@am2bmarketing.co.th>
2021-02-01 13:39:44 +0700

    added travis deploy config

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello World! Keng!
+          Hello Am2b!
         </p>
         <a
           className="App-link"
